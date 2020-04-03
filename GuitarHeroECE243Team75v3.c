b02a1d53ad46e311589b16b6753c8087cdd2fd91
khodawan <k.khodawandi@mail.utoronto.ca>
2020-04-03 12:33:10 -0400

    Update GuitarHeroECE243Team75v3.c
    Fixed issue with doubling score count during game play. Fixed glitch with score_menu so that previous game_menu screen no longer appears. Score menu now correctly shows most recent score on screen. Added a reset function that resets all tap elements and other game_info variables after a game has been played and user chooses option to replay.

--- GuitarHeroECE243Team75v3.c
+++ GuitarHeroECE243Team75v3.c
@@ -12,39 +12,26 @@
 #include <math.h>
 
 //address_map.h header file
-/* This files provides address values that exist in the system */
+// relevant addresses used in code 
 
 #define BOARD                 "DE1-SoC"
 
-/* Memory */
-#define DDR_BASE              0x00000000
-#define DDR_END               0x3FFFFFFF
-#define A9_ONCHIP_BASE        0xFFFF0000
-#define A9_ONCHIP_END         0xFFFFFFFF
-#define SDRAM_BASE            0xC0000000
-#define SDRAM_END             0xC3FFFFFF
-#define FPGA_ONCHIP_BASE      0xC8000000
-#define FPGA_ONCHIP_END       0xC803FFFF
-#define FPGA_CHAR_BASE        0xC9000000
-#define FPGA_CHAR_END         0xC9001FFF
-
 /* Cyclone V FPGA devices */
-#define LEDR_BASE             0xFF200000
-#define HEX3_HEX0_BASE        0xFF200020
-#define HEX5_HEX4_BASE        0xFF200030
-#define SW_BASE               0xFF200040
-#define KEY_BASE              0xFF200050
 #define PS2_BASE              0xFF200100
 #define TIMER_BASE            0xFF202000
 #define PIXEL_BUF_CTRL_BASE   0xFF203020
 #define CHAR_BUF_CTRL_BASE    0xFF203030
 #define AUDIO_BASE            0xFF203040
 
+#define SDRAM_BASE            0xC0000000
+#define FPGA_ONCHIP_BASE      0xC8000000
+#define FPGA_CHAR_BASE        0xC9000000
+
 /* ARM A9 MPCORE devices */
 #define   PERIPH_BASE         0xFFFEC000    // base address of peripheral devices
 #define   MPCORE_PRIV_TIMER   0xFFFEC600    // PERIPH_BASE + 0x0600
 
-// keyboard button scan codes 
+// relevant keyboard button scan codes 
 #define Q		0x15
 #define W		0x1D
 #define E		0x24
@@ -57,6 +44,8 @@
 #define N_5     0x2E
 #define ENTER	0x5A 
 #define SPACE	0x29 
+
+#define oneSec    200000000
 
 //initialize functions to be used in main
 void plot_pixel(int x1,int y1, short int pixel_colour);
@@ -80,7 +69,10 @@ void read_keyboard_game();
 bool read_keyboard_score();
 void write_int(int x,int y, int num);
 int count_digits(int num);
+void reset(); 
+void wait_state_play();
 
+// structure that keeps track of columns with tap element(s) within bounds for points if tapped 
 struct points{
 	bool one;
 	bool two;
@@ -88,7 +80,6 @@ struct points{
 	bool four; 
 	bool five; 
 }; 
-
 
 //structure containing each colour value so that it is easier to draw images
 struct colours{
@@ -103,77 +94,6 @@ struct colours{
     short int yellow;
     short int grey;
 };
-
-//info about the current game
-struct  current
-{
-    int song_num;
-    char song_name[30];
-    int current_score;
-    int difficulty_level;
-    int current_song_length;
-    int tempo;
-};
-
-//define the current state of the game
-struct game_data{
-    //0-> start menu, 2->game_menu, 3-> score menu
-    int current_state;
-    //need to use switch case to determine what things to initialize according to states
-    //depending on difficulty, tempo_multiplier 
-    double difficulty_tempo_multiplier[4];
-    //each of the song names respectively
-    char song_names[4][30]; 
-    //a vector of the song tempos for each song respectively
-    double song_default_tempo[4];
-    int drop_speed;
-
-    //can have up to 50 tap elements on the screen
-    int tap_element_x [50];
-    int tap_element_y [50];
-    int tap_element_int [50];
-
-    //rate of timer
-    int timer_rate;// timeout = 1/(200 MHz) x 200x10^6 = 1 sec
-    int positions [5];
-    short int tap_element_colours[5];
-    int high_score [4][4];
-    int last_score;
-    struct current current_info;
-    int song_length_sec[4];
-    int pressed_button;
-	struct points check_points; 
-};
-
-
-
-
-//initialize the game data, defaults to start_menu, easy, song_1;
-struct game_data game_info = {
-                            .song_names = {"song_1", "song_2", "song_3", "song_4"},
-                            .song_default_tempo = {100,100,100,100}, 
-                            .difficulty_tempo_multiplier = {1,4,8,12},
-                            .drop_speed = 4, 
-                            .tap_element_x = 0,
-                            .tap_element_y = 0,
-                            .tap_element_int = 0,
-                            .timer_rate = 200000000,
-                            .positions = {(43 + 4 + 9*4*1) , (43 + 4 + 9*4*2), (43 + 4 + 9*4*3), (43 + 4 + 9*4*4), (43 + 4 + 9*4*5) },
-                            //blue, green, red, yellow, pink
-                            .tap_element_colours = {0x555F, 0x5FA5, 0xf888,0xfff0,0xf833},
-                            .current_info.song_num = 1,
-                            .current_info.song_name = "song_1",
-                            .current_info.current_score = 0,
-                            .current_info.difficulty_level = 1,
-                            .current_info.tempo = 200000000,
-                            .song_length_sec = {30,60,90,120},
-                            .pressed_button = 0,
-							.check_points.one = false, 
-							.check_points.two = false,
-							.check_points.three = false,
-							.check_points.four = false,
-							.check_points.five = false
-                            };
 
 //set respective colours to their value
 struct colours colour = {
@@ -190,40 +110,94 @@ struct colours colour = {
                     };
 
 
-struct KEY_data {
-    int KEY0_pressed;
-    int KEY1_pressed;
-    int KEY2_pressed;
-    int KEY3_pressed; 
-}; 
+//info about the current game
+struct  current
+{
+    int song_num;
+    char song_name[30];
+    int current_score;
+    int difficulty_level;
+    int current_song_length;
+    int tempo;
+};
 
-struct KEY_data KEY_info = {
-                            .KEY0_pressed = 0,
-                            .KEY1_pressed = 0,
-                            .KEY2_pressed = 0,
-                            .KEY3_pressed = 0
-                        }; 
+//define the current state of the game
+struct game_data{
+    
+    int current_state;      // 0-> start menu, 2->game_menu, 3-> score menu
+                            // need to use switch case to determine what things to initialize according to states
+    
+    double difficulty_tempo_multiplier[4];  //depending on difficulty, tempo_multiplier 
+    
+    char song_names[4][30];         // each of the song names respectively
+   
+    double song_default_tempo[4];   // song tempos for all 4 songs 
+    int drop_speed;                 // controlled by selected difficulty 
+     int song_length_sec[4];
 
-//pixel_buffer_start points to the pixel buffer address
-volatile int pixel_buffer_start; 
+    int timer_rate;     // timeout = 1/(200 MHz) x 200x10^6 = 1 sec
+
+    //can have up to 50 tap elements on the screen
+    int tap_element_x [50];
+    int tap_element_y [50];
+    int tap_element_int [50];
+    short int tap_element_colours[5];
+    int positions [5];      // x pos that tap elements can exist in (corresponding to 5 columns on guitar)
+    
+    int high_score [4][4];      // array tracking best score for 4 difficulty levels and 4 song choices 
+    int last_score;
+    struct current current_info;
+   
+    int pressed_button;             // to track last keyboard button pushed 
+	struct points check_points;     // to track buttons that earn points if pushed 
+};
+
+
+//initialize game data, defaults to start_menu, easy, song_1;
+struct game_data game_info = {
+                            .song_names = {"song_1", "song_2", "song_3", "song_4"},
+                            .song_default_tempo = {100,100,100,100}, 
+                            .difficulty_tempo_multiplier = {1,4,8,12},
+                            .drop_speed = 4, 
+                            .tap_element_x = 0,
+                            .tap_element_y = 0,
+                            .tap_element_int = 0,
+                            .timer_rate = oneSec,
+                            .positions = {(43 + 4 + 9*4*1) , (43 + 4 + 9*4*2), (43 + 4 + 9*4*3), (43 + 4 + 9*4*4), (43 + 4 + 9*4*5) },
+                            .tap_element_colours = {0x555F, 0x5FA5, 0xf888,0xfff0,0xf833}, //blue, green, red, yellow, pink
+                            .current_info.song_num = 1,
+                            .current_info.song_name = "song_1",
+                            .current_info.current_score = 0,
+                            .current_info.difficulty_level = 1,
+                            .current_info.tempo = oneSec,
+                            .song_length_sec = {30,60,90,120},
+                            .pressed_button = 0,
+							.check_points.one = false, 
+							.check_points.two = false,
+							.check_points.three = false,
+							.check_points.four = false,
+							.check_points.five = false
+                            };
+
+/* global variables */ 
+
+volatile int pixel_buffer_start;  //pixel_buffer_start points to the pixel buffer address
 volatile int * pixel_ctrl_ptr;
-//timer
-volatile int * MPcore_private_timer_ptr = (int *)MPCORE_PRIV_TIMER;
+volatile int * MPcore_private_timer_ptr = (int *)MPCORE_PRIV_TIMER; //timer
 
-
+/* start of game */ 
 int main(void){
     //pointer to the pixel controller address
-    pixel_ctrl_ptr = (int *)0xFF203020;
-    volatile char * character_buffer = (char *) (0xc9000000);
+    pixel_ctrl_ptr = (int *)PIXEL_BUF_CTRL_BASE;
+    volatile char * character_buffer = (char *) (FPGA_CHAR_BASE);
     /* Read location of the pixel buffer from the pixel buffer controller */
     pixel_buffer_start = *pixel_ctrl_ptr;
-
 
     //keep the program running and prevent the program from ending
     //clear the screen initially to set the background to black
     plot_pixel(0,0,colour.grey);
 
-    *(pixel_ctrl_ptr + 1) = 0xC8000000; // first store the address in the back buffer
+    *(pixel_ctrl_ptr + 1) = FPGA_ONCHIP_BASE; // first store the address in the back buffer
 
     /* now, swap the front/back buffers, to set the front buffer location */
     wait_state();
@@ -233,7 +207,7 @@ int main(void){
     clear_screen(); // pixel_buffer_start points to the pixel buffer
 
     /* set back pixel buffer to start of SDRAM memory */
-    *(pixel_ctrl_ptr + 1) = 0xC0000000;
+    *(pixel_ctrl_ptr + 1) = SDRAM_BASE;
     pixel_buffer_start = *(pixel_ctrl_ptr + 1); // we draw on the back buffer
      
     game_info.current_state = 1;
@@ -248,9 +222,9 @@ int main(void){
         if (game_info.tap_element_y[0]>=235)
             game_info.tap_element_y[0] = 0;
         
-        game_info.tap_element_y[0] = game_info.tap_element_y[0] + game_info.drop_speed;   
+        game_info.tap_element_y[0] = game_info.tap_element_y[0] + game_info.drop_speed;  //KK question: what does this do?  
 
-        //wait and swap front and back bufferes for vsync   
+        //wait and swap front and back buffers for vsync   
         wait_state();
 
         //set new back buffer
@@ -297,7 +271,7 @@ void draw_tap_element(int x1, int y1, short int element_colour){
  */
 void write_char(int x, int y, char c) {
   // VGA character buffer
-  volatile char * character_buffer = (char *) (0xc9000000 + (y<<7) + x);
+  volatile char * character_buffer = (char *) (FPGA_CHAR_BASE + (y<<7) + x);
   *character_buffer = c;
 }
 
@@ -439,13 +413,18 @@ void draw_game_menu(){
         //draw the menu name
         char menu_name[] = "Game Menu";
         draw_string(1, 1, menu_name);
+
+        // draw score 
         draw_string(79-10, 1, "Score:");
         write_int(79-10, 3, game_info.current_info.current_score);
+
+        // show key pressed 
         draw_string(79-10, 5, "Last Key");
         draw_string(79-10, 6, "Pressed:");
         write_int(79-10, 8, game_info.pressed_button);
 
-        char check[] = "Checked:"; 
+        // show checker for when tap element crosses bounds for points 
+        char check[] = "Column for points:"; 
         draw_string(69, 10, check);
         write_int(69, 11, game_info.check_points.one);
         write_int(69, 12, game_info.check_points.two);
@@ -467,7 +446,7 @@ void draw_game_menu(){
         play_game(num_elements);
         
 
-        for (int k =0; k < num_elements + 1; k++){
+        for (int k =0; k < num_elements; k++){
             if(game_info.tap_element_y[k] <= 239-8 && game_info.tap_element_x[k] != 0){
                 game_info.tap_element_y[k] += 4;
             }
@@ -494,7 +473,7 @@ void draw_game_menu(){
         if(num_elements<50) 
             num_elements ++;
         
-        wait_state();
+        wait_state_play();
         //set new back buffer
         pixel_buffer_start = *(pixel_ctrl_ptr + 1);
 
@@ -509,6 +488,7 @@ void draw_game_menu(){
             time_left -= 1/12;
         else break;
     }
+    
     clear_screen();
     wait_state();
     //set new back buffer
@@ -525,7 +505,7 @@ void play_game(int upper_bound){
     *(MPcore_private_timer_ptr + 3) = 1; // reset timer flag bit
 
     int random_init = 0;
-    for(int k = 0; k < upper_bound + 1; k++){
+    for(int k = 0; k < upper_bound ; k++){
         if(game_info.tap_element_x[k] == 0){
             //1/20 chance of adding new element
             if (rand()%(20) + 1 == 1){
@@ -537,7 +517,7 @@ void play_game(int upper_bound){
             }   
         }
     }
-    for (int k =0; k < upper_bound + 1; k++){
+    for (int k =0; k < upper_bound; k++){
         if(game_info.tap_element_y[k] <= 239-8 && game_info.tap_element_x[k] != 0)  { 
             draw_tap_element(game_info.tap_element_x[k],game_info.tap_element_y[k],game_info.tap_element_colours[game_info.tap_element_int[k]]);
             if (game_info.tap_element_y[k] >= 239 - 16) {
@@ -565,6 +545,8 @@ void play_game(int upper_bound){
 void draw_score_menu(){
     bool change_menu = false;
     while (change_menu == false){
+
+        clear_screen(); // to get rid of old game_menu screen 
         // Main header - game title 
         char screen_title[] = "* Thanks for Playing *"; 
         char game_difficulty[10] = "";
@@ -582,7 +564,7 @@ void draw_score_menu(){
 
         //print the score the user had from playing the game
         draw_string(80/2 - strlen("Your Score Was: ")/2,60/3 + 4 , "Your Score Was: ");
-        write_int(80/2 - count_digits(current_high_score)/2,60/3 + 6, current_high_score);
+        write_int(80/2 - count_digits(current_score)/2,60/3 + 6, current_score);
 
                 
 
@@ -593,7 +575,7 @@ void draw_score_menu(){
         }
         else if(current_score<current_high_score){
             int score_difference = current_score - current_high_score;
-            draw_string(80/2 - strlen("You Beat the High Score By: ")/2,60/3 + 8, "You Beat the High Score By: ");
+            draw_string(80/2 - strlen("You were below the High Score By: ")/2,60/3 + 8, "You were below the High Score By: ");
             write_int(80/2 - count_digits(score_difference)/2,60/3 + 10, score_difference);
         }
         else{
@@ -689,7 +671,25 @@ void clear_line(int x1, int y1, int x2, int y2){
 
 void wait_state(){
     //set the pixel_ctrl_ptr to point to the pixel_ctrl_ptr address
-    volatile int * pixel_ctrl_ptr = (int *)0xFF203020;
+    volatile int * pixel_ctrl_ptr = (int *)PIXEL_BUF_CTRL_BASE;
+
+    //status points to the status register to check for synchronization
+    volatile int * status =(int *)0xFF20302C;
+
+    *pixel_ctrl_ptr = 1;
+
+    //if the value at status is 1, keep reading. exit once synchronized
+    while((*status & 0x01) != 0) {
+        status = status; //keep reading status
+    } 
+
+    //return out of wait_state when s = 1
+    return;
+}  
+
+void wait_state_play(){
+    //set the pixel_ctrl_ptr to point to the pixel_ctrl_ptr address
+    volatile int * pixel_ctrl_ptr = (int *)PIXEL_BUF_CTRL_BASE;
 
     //status points to the status register to check for synchronization
     volatile int * status =(int *)0xFF20302C;
@@ -704,7 +704,7 @@ void wait_state(){
     read_keyboard_game(); 
     //return out of wait_state when s = 1
     return;
-}    
+}
 //draw_line function, draws a line between 2 points in the specified pixel colour
 void draw_line(int x1, int y1, int x2, int y2, short int line_colour){
     
@@ -907,32 +907,32 @@ void read_keyboard_game(){
     if (byte3 == N_1){
         game_info.pressed_button = 1;
 		if (game_info.check_points.one == true)
-			game_info.current_info.current_score += 10; 
+			game_info.current_info.current_score += 1; 
     }
     // 2 pressed 
     else if (byte3 == N_2){
         game_info.pressed_button = 2;
         if (game_info.check_points.two == true)
-            game_info.current_info.current_score += 10; 
+            game_info.current_info.current_score += 1; 
 		
     }
     // 3 pressed 
     else if (byte3 == N_3){
         game_info.pressed_button = 3;
         if (game_info.check_points.three == true)
-            game_info.current_info.current_score += 10; 
+            game_info.current_info.current_score += 1; 
     }
     // 4 pressed 
     else if (byte3 == N_4){
          game_info.pressed_button = 4;
         if (game_info.check_points.four == true)
-           game_info.current_info.current_score += 10; 
+           game_info.current_info.current_score += 1; 
     }
     // 5
     else if (byte3 ==N_5){
         game_info.pressed_button = 5;
         if (game_info.check_points.five == true)
-           game_info.current_info.current_score += 10; 
+           game_info.current_info.current_score += 1; 
     }
     else {
         game_info.pressed_button = 0; // reset so there is no memory of last clicked key 
@@ -958,13 +958,44 @@ bool read_keyboard_score(){
 		byte2 = byte3;
         byte3 = PS2_data & 0xFF;
     } 
-    // 1 pressed for easy 
+    // returns to start_menu 
     if (byte3 == T){
         game_info.current_state = 1;
+        reset(); 
         return true;
     }
     else {
         return false; 
     }
 	
+}
+
+void reset(){
+
+    game_info.last_score = game_info.current_info.current_score; 
+    game_info.current_info.current_score = 0; 
+	
+	for (int i=0; i<3; i++)
+		game_info.song_default_tempo[i] = 100; 
+    
+	game_info.drop_speed = 4;
+	
+	for (int i=0; i<50;i++){
+		game_info.tap_element_x[i] = 0;
+    	game_info.tap_element_y[i] = 0;
+    	game_info.tap_element_int[i] = 0;
+	} 
+    
+	game_info.timer_rate = oneSec;
+    game_info.current_info.song_num = 1;
+    
+    game_info.current_info.difficulty_level = 1;
+    game_info.current_info.tempo = oneSec;
+    game_info.pressed_button = 0;
+    game_info.check_points.one = false;
+    game_info.check_points.two = false;
+    game_info.check_points.three = false;
+    game_info.check_points.four = false;
+    game_info.check_points.five = false;
+
 }
